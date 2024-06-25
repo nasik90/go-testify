f8a6f68194698fbb370619b5658389d0b725260e
nasik90 <anasnazm@gmail.com>
2024-06-25 18:44:22 +0300

    тесты через пакет testify

--- main_test.go
+++ main_test.go
@@ -5,27 +5,20 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMainHandlerStatusOk(t *testing.T) {
-	//totalCount := 4
-	//city := "moscow"
-	//reqString := fmt.Sprintf("/cafe?count=%1&city=%2", totalCount, city)
-	req := httptest.NewRequest("GET", "/cafe?count=4&city=moscow", nil) // здесь нужно создать запрос к сервису
+	req := httptest.NewRequest("GET", "/cafe?count=4&city=moscow", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	// здесь нужно добавить необходимые проверки
+	assert.Equal(t, responseRecorder.Code, http.StatusOK)
 
-	if status := responseRecorder.Code; status != http.StatusOK {
-		t.Errorf("expected status code: %d, got %d", http.StatusBadRequest, status)
-	}
-
-	if responseRecorder.Body.String() == "" {
-		t.Errorf("expected not empty body")
-	}
+	assert.NotEmpty(t, responseRecorder.Body.String())
 }
 
 func TestMainHandlerWrongCity(t *testing.T) {
@@ -35,14 +28,10 @@ func TestMainHandlerWrongCity(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	if status := responseRecorder.Code; status != http.StatusBadRequest {
-		t.Errorf("expected status code: %d, got %d", http.StatusBadRequest, status)
-	}
+	assert.Equal(t, responseRecorder.Code, http.StatusBadRequest)
 
 	expected := `wrong city value`
-	if responseRecorder.Body.String() != expected {
-		t.Errorf("expected body: %s, got %s", expected, responseRecorder.Body.String())
-	}
+	assert.Equal(t, responseRecorder.Body.String(), expected)
 }
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
@@ -56,7 +45,5 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	cafe := strings.Split(responseRecorder.Body.String(), ",")
 	lenCafe := len(cafe)
 
-	if lenCafe != totalCount {
-		t.Errorf("expected count: %d, got %d", totalCount, lenCafe)
-	}
+	assert.Equal(t, lenCafe, totalCount)
 }
